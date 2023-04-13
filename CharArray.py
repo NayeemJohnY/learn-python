b713548d907afc75329e397b29b12f2681ff7581
NayeemJohn <44828643+NayeemJohnY@users.noreply.github.com>
2023-04-13 17:21:03 +0530

    Update CharArray.py

--- CharArray.py
+++ CharArray.py
@@ -1,5 +1,5 @@
 Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "creditss" or "license()" for more information.
+Type "help", "copyright", "credits" or "license()" for more information.
 >>> word="python"
 >>> word[-6]
 'p'
